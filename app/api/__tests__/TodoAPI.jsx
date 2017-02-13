76ceacaf0c15783f733afab23ead87c17ab49d3f
Ludo Bermejo Fernández <Ludo Bermejo Fernndez>
2017-02-14 00:06:42 +0100

    Local storage with tests

--- app/api/__tests__/TodoAPI.jsx
+++ app/api/__tests__/TodoAPI.jsx
@@ -15,9 +15,9 @@ describe('TodoAPI', () => {
     expect(TodoAPI.getTodos()).toEqual([]);
   });
 
-  it('should return an array if you put a array ', () => {
+  it('should store valid data if you put a array ', () => {
     TodoAPI.setTodos(['demo', 'demo2']);
-    expect(TodoAPI.getTodos()).toEqual(['demo', 'demo2']);
+    expect(JSON.parse(localStorage.getItem('todos'))).toEqual(['demo', 'demo2']);
 
     const complexArray = [
       {
@@ -43,6 +43,39 @@ describe('TodoAPI', () => {
     ];
 
     TodoAPI.setTodos(complexArray);
+
+    expect(JSON.parse(localStorage.getItem('todos'))).toEqual(complexArray);
+  });
+
+  it('should return valid data if you put a array ', () => {
+
+    window.localStorage.setItem('todos', JSON.stringify(['demo', 'demo2']) );
+    expect(TodoAPI.getTodos()).toEqual(['demo', 'demo2']);
+
+    const complexArray = [
+      {
+        id: uuid(),
+        value: 'first',
+        completed: false
+      },
+      {
+        id: uuid(),
+        value: 'Second',
+        completed: false
+      },
+      {
+        id: uuid(),
+        value: 'Third',
+        completed: false
+      },
+      {
+        id: uuid(),
+        value: 'Fourth',
+        completed: false
+      }
+    ];
+
+    window.localStorage.setItem('todos', JSON.stringify(complexArray));
 
     expect(TodoAPI.getTodos()).toEqual(complexArray);
   });
